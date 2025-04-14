d1e4f29365e7d5628bae5a402a97af3549cfe4d9
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-13 23:34:07 -0600

    terminado

---
+++ Entrenamiento 100 ejercicios/ejercicio 2.py
@@ -0,0 +1,8 @@
+'''Calcula el área de un círculo'''
+
+import math
+
+print("\nIngrese el radio del círculo: ", end = '')
+radio = int(input())
+area = math.pi * radio ** 2
+print("\nEl área del círculo es: ", area)
