6d7ea44035e59c60ca5411b6dd4e65ffe0e9ebfb
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-13 23:40:54 -0600

    terminado

--- Entrenamiento 100 ejercicios/ejercicio 3.py
+++ Entrenamiento 100 ejercicios/ejercicio 3.py
@@ -1,1 +1,8 @@
 '''Concatena dos cadenas de texto'''
+
+print("Escriba algo: ", end = '')
+cadena1 = input()
+print("Escriba otra cosa: ", end = '')
+cadena2 = input()
+concatenacion = cadena1 + " " + cadena2
+print("La cadena es: ", concatenacion)
