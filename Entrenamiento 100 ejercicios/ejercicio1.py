0861ecc494f4ef1cf6ea9be0db3817dddc7c9bf1
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-13 23:28:54 -0600

    terminado

---
+++ Entrenamiento 100 ejercicios/ejercicio1.py
@@ -0,0 +1,8 @@
+'''Suma dos números y muestra su resultado'''
+
+print("\nIngrese un número: ", end = '')
+num1 = int(input())
+print("\nIngrese otro número: ", end = '')
+num2 = int(input())
+resultado = num1 + num2
+print("\nLa suma de los números es ", resultado)
